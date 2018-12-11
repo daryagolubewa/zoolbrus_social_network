116e13addc38a9dedf251716152bb1b553fadba6
Michael Klishevich <busation@gmail.com>
2018-12-11 10:50:45 +0300

    fix(react-bootstrap): fix react bootstrap and add menu

--- src/client/components/app/app.jsx
+++ src/client/components/app/app.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import {
-  Button, Col, Row, Grid
+  Button, Col, Row, Grid, Navbar, Nav, NavItem, NavDropdown, MenuItem
 } from 'react-bootstrap';
-// import { connect } from 'react-redux';
-// import { bindActionCreators } from 'redux';
-// import Type from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import Type from 'prop-types';
 import { Link } from 'react-router-dom';
-// import { push } from 'connected-react-router';
+import { push } from 'connected-react-router';
 // import elbrusImg from './elbrus.png';
 import { PAGES } from '../../routes/pages';
 // import { bemClassNameFactory } from '../../utils/bem';
@@ -31,56 +31,26 @@ import './app.css';
 //   isPostsFetching: selectIsPostsFetching(state)
 // });
 
-// Alternative Way to map dispatch to props
-// const mapDispatchToProps = dispatch => ({
-//   sayBye: () => dispatch(sayByeAC()),
-//   sayHi: () => dispatch(sayHiAC()),
-//   doRoute: page => dispatch(push(page)),
-//   fetchUserStart: () => dispatch(fetchUserStartAC()),
-//   fetchUserSuccess: user => dispatch(fetchUserSuccessAC(user)),
-//   fetchUserError: () => dispatch(fetchUserErrorAC())
-// });
+const mapDispatchToProps = dispatch => bindActionCreators({
+  doRoute: push
+}, dispatch);
 
-// const mapDispatchToProps = dispatch => bindActionCreators({
-//   sayBye: sayByeAC,
-//   sayHi: sayHiAC,
-//   doRoute: push,
-//   fetchUserStart: fetchUserStartAC,
-//   fetchUserSuccess: fetchUserSuccessAC,
-//   fetchUserError: fetchUserErrorAC,
-//   fetchPosts: fetchPostsThunkAC
-// }, dispatch);
-
-export default class App extends Component {
-  // static propTypes = {
-  //   appName: Type.string,
-  //   children: Type.node.isRequired,
-  //   say: Type.string,
-  //   pathname: Type.string,
-  //   userInfo: Type.shape({
-  //     name: Type.string,
-  //     email: Type.string
-  //   }),
-  //   isUserFetching: Type.bool,
-  //   sayHi: Type.func,
-  //   sayBye: Type.func,
-  //   doRoute: Type.func,
-  //   posts: Type.array,
-  //   isPostsFetching: Type.bool,
-  //   fetchUserStart: Type.func,
-  //   fetchUserSuccess: Type.func,
-  //   fetchUserError: Type.func,
-  //   fetchPosts: Type.func
-  // };
+class App extends Component {
+  static propTypes = {
+    doRoute: Type.func.isRequired
+  };
 
   // static defaultProps = {
-  //   appName: 'Default Name',
-  //   posts: []
+  //   name: 'Default Name',
   // };
-  //
 
   state = {
     show: false
+  };
+
+  handleOpenPage = path => () => {
+    const { doRoute } = this.props;
+    doRoute(path);
   };
 
   render() {
@@ -89,53 +59,77 @@ export default class App extends Component {
     } = this.props;
     // console.log(this.props);
     return (
-      <div className='container-fluid'>
-        <Grid>
-          <Row>
-            <Col xs={4} xsOffset={4} lg={4} lgOffset={9} md={8} mdOffset={8}>
-              <div className='button-toolbar'>
-                <Link to={PAGES.signup.path}>
-                  <Button className='form-buttons' bsStyle='primary'>Зарегистрироваться</Button>
+      <div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#" onClick={this.handleOpenPage(PAGES.home.path)}>Zoolbrus</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="#" onClick={this.handleOpenPage(PAGES.users.teachers.path)}>
+              Наши преподаватели
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Link
+            </NavItem>
+            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider/>
+              <MenuItem eventKey={3.4}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar>
+        <div className='container-fluid'>
+          <Grid>
+            <Row>
+              <Col xs={4} xsOffset={4} lg={4} lgOffset={9} md={8} mdOffset={8}>
+                <div className='button-toolbar'>
+                  <Link to={PAGES.signup.path}>
+                    <Button className='form-buttons' bsStyle='primary'>Зарегистрироваться</Button>
+                  </Link>
+                  <Link to={PAGES.signin.path}>
+                    <Button className='form-buttons' bsStyle='primary'>Войти</Button>
+                  </Link>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={4} lg={12} md={8} className='main-container'>
+                <Link to={PAGES.users.teachers.path}>
+                  <div className='menu-text'>
+                    Наши преподаватели
+                  </div>
                 </Link>
-                <Link to={PAGES.signin.path}>
-                  <Button className='form-buttons' bsStyle='primary'>Войти</Button>
+                <Link to={PAGES.users.students.path}>
+                  <div className='menu-text'>
+                    Наши студенты
+                  </div>
                 </Link>
-              </div>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={4} lg={12} md={8} className='main-container'>
-              <Link to={PAGES.users.teachers.path}>
-                <div className='menu-text'>
-                  Наши преподаватели
-                </div>
-              </Link>
-              <Link to={PAGES.users.students.path}>
-                <div className='menu-text'>
-                  Наши студенты
-                </div>
-              </Link>
-              <Link to={PAGES.about.path}>
-                <div className='menu-text'>
-                  О нас
-                </div>
-              </Link>
-              <Link to={PAGES.feedback.path}>
-                <div className='menu-text'>
-                  Задать вопрос
-                </div>
-              </Link>
-            </Col>
-          </Row>
-          {children}
-        </Grid>
+                <Link to={PAGES.about.path}>
+                  <div className='menu-text'>
+                    О нас
+                  </div>
+                </Link>
+                <Link to={PAGES.feedback.path}>
+                  <div className='menu-text'>
+                    Задать вопрос
+                  </div>
+                </Link>
+              </Col>
+            </Row>
+            {children}
+          </Grid>
+        </div>
       </div>
     );
   }
 }
 
-// const VisibleApp = connect(
-//   mapStateToProps,
-//   mapDispatchToProps
-// )(App);
-// export default VisibleApp;
+const VisibleApp = connect(
+  null,
+  mapDispatchToProps
+)(App);
+export default VisibleApp;
